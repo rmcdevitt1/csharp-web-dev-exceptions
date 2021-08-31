2dc18d2c2bd2b61ff4e4caacb494b875ac5b579d
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-08-31 16:00:43 -0500

    DONE

--- csharp-web-dev-exceptions/Program.cs
+++ csharp-web-dev-exceptions/Program.cs
@@ -7,21 +7,52 @@ namespace csharp_web_dev_lsn9exceptions
 {
     class Program
     {
-        /*
+        
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            if (y == 0)
+            {
+                throw new ArgumentOutOfRangeException("Cannot divide by zero.");
+            }
+            else
+            {
+                return x / y;
+            }
         }
 
         static int CheckFileExtension(string fileName)
         {
             // Write your code here!
+            if (fileName == "" || fileName == null)
+            {
+                throw new ArgumentNullException("No file!");
+            }
+            else
+            {
+                if (fileName.EndsWith(".cs"))
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
         }
-        */
+
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
+            try
+            {
+                Console.WriteLine(Divide(12.0, 4.0));
+                Console.WriteLine(Divide(10.0, 0.0));
+            } catch(ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Cannot divide by zero.");
+            }
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
@@ -30,7 +61,18 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
 
+            foreach (KeyValuePair<string, string> pair in students)
+            {
+                try
+                {
+                    Console.WriteLine(pair.Key + ": " + CheckFileExtension(pair.Value));
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine(pair.Key + ": " + "No file!");
+                }
 
+            }
         }
     }
 }
